707ad61a328974e24e276c9a27e245b9aae27b3f
root <root@ip-10-0-29-18.ec2.internal>
2023-02-09 10:15:43 +0000

    new file added

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "node-hello",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/arihant-sirpi/sample-nodejs.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/arihant-sirpi/sample-nodejs.git/issues"
+  },
+  "homepage": "https://github.com/arihant-sirpi/sample-nodejs.git#readme"
+}
